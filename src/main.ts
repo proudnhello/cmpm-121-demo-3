eab2a78847fae8cc634b2260ddb191ef110de234
proudnhello <92887967+proudnhello@users.noreply.github.com>
2024-11-04 15:28:37 -0800

    added button for brace

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+import "./style.css";
+
+const app = document.getElementById("app")!;
+
+const button = document.createElement("button");
+button.textContent = "Click me!";
+button.onclick = () => {
+  alert("Hello, world!");
+};
+app.appendChild(button);
